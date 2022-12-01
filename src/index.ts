831a725bdb1352aa4ff5e6538f9d8b4bbd513c43
ConnorBStill <connor@lamarsoftware.io>
2022-12-01 02:17:42 -0400

    Create REsponse Builder; Mock data for last-messages endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,63 @@
-import express from 'express';
-// const express = require( "express" );
+import express, { Request, Response } from 'express';
+import { Server } from 'ws';
+// const cors=require("cors");
+import cors from 'cors';
+
+import { responseBuilder } from './utils/responseBuilder';
+
 const app = express();
 const port = 8080; // default port to listen
+const sockserver = new Server({ port: 443 });
+
+const corsOptions = {
+   origin: '*', 
+   credentials: true,            //access-control-allow-credentials:true
+   optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 // define a route handler for the default home page
-app.get( "/", (req: any, res: any) => {
-    res.send( "Hello world!" );
+
+// app.use((req, res) => res.json({ data: 'DATAAA' }));
+
+// sockserver.on('connection', (ws) => {
+//     console.log('New client connected!'); 
+//     ws.on('close', () => console.log('Client has disconnected!'));
+// });
+
+app.get('/last-messages', (req: any, res: any) => {
+    try {
+        const messages = [
+            {
+                body: 'Want to see a movie friday?',
+                from: 'John',
+                date: ''
+            },
+            {
+                body: 'The new Prime password is password123456',
+                from: 'The Mom',
+                date: ''
+            },
+            {
+                body: "Yeah you should get Scarlet if she's getting Violet",
+                from: 'Jarrod',
+                date: ''
+            },
+            {
+                body: 'Ok',
+                from: 'The Dad',
+                date: ''
+            }
+        ];
+    
+        res.json(responseBuilder(messages, false));
+    } catch (err) {
+        res.json(responseBuilder(null, true));
+    }
 });
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${port}` );
+app.listen(port, () => {
+    console.log( `server started at http://localhost:${port}`);
 });
---
+++ src/utils/responseBuilder.ts
@@ -0,0 +1,3 @@
+export const responseBuilder = (data: any, error: boolean): { data: any, error: boolean } => {
+  return { data, error }
+}
